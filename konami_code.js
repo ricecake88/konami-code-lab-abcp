7a5cf5b1754ff1b3f5ff0446dd382712dcd214bf
Grace Shih <vocalise@gmail.com>
2018-01-20 18:33:10 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,16 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+var konami_index = 0;
 
 function init() {
   // Write your JavaScript code inside the init() function
+  document.body.addEventListener('keydown', react);
+}
 
+function react(e) {
+  if (e.which === code[konami_index]) {
+    konami_index++;
+  }
+  else {
+    konami_index = 0;
+  }
 }
