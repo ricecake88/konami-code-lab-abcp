7668e12ca009b652551b2c9f20bcafbe0063e64f
Grace Shih <vocalise@gmail.com>
2018-01-20 18:37:12 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -8,8 +8,12 @@ function init() {
 }
 
 function react(e) {
-  if (e.which === code[konami_index]) {
+  const key = parseInt(e.detail || e.which);
+  if (key === code[konami_index]) {
     konami_index++;
+    if (konami_index == code.length) {
+      alert("Congratulations!");
+    }
   }
   else {
     konami_index = 0;
